c384df0f0e4eb1c4274c6a794b6d9beb3e0867d8
73N37 <x0d4r4p@proton.me>
2025-09-24 13:45:47 +0200

    removed id from contsructer

--- src/main/java/app/DTOs/PoemDTO.java
+++ src/main/java/app/DTOs/PoemDTO.java
@@ -1,4 +1,35 @@
 package app.DTOs;
 
+import app.entities.Poem;
+
 public record PoemDTO() {
+    static Integer id;
+    static String title;
+    static String poem;
+    static String style;
+
+    public static PoemDTO convertToDTO(Poem poem){
+        PoemDTO result = new PoemDTO();
+        result.id = poem.getId();
+        result.title = poem.getTitle();
+        result.poem = poem.getPoem();
+        result.style = poem.getStyle();
+        return result;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getPoem(){
+        return poem;
+    }
+
+    public String getStyle(){
+        return style;
+    }
 }
