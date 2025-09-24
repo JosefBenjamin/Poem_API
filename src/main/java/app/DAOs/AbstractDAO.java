284893efbb27203855e6fe81547a4ad4b1690f0a
73N37 <x0d4r4p@proton.me>
2025-09-24 14:21:26 +0200

    created a delete(Integer id)

--- src/main/java/app/DAOs/AbstractDAO.java
+++ src/main/java/app/DAOs/AbstractDAO.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EntityManagerFactory;
 public class AbstractDAO<Entity, DTO, ID> implements IDAO<Entity, DTO, ID>{
 
     protected static EntityManagerFactory emf;
+    protected Class<Entity> entityClass;
 
     AbstractDAO(EntityManagerFactory emf) {
         this.emf = emf;
@@ -18,5 +19,23 @@ public class AbstractDAO<Entity, DTO, ID> implements IDAO<Entity, DTO, ID>{
            em.getTransaction().commit();
        }
        return entity;
+    }
+
+    public void delete(ID id) {
+        try(EntityManager em = emf.createEntityManager()) {
+            String jpql = "DELETE FROM " + entityClass.getSimpleName() + " a WHERE a.id = :id";
+            em.createQuery(jpql).setParameter("id",id)
+                    .executeUpdate();
+            em.getTransaction().commit();
+        }
+    }
+
+    public Entity update(Entity entity) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.merge(entity);
+            em.getTransaction().commit();
+        }
+        return entity;
     }
 }
--- src/main/java/app/DAOs/IDAO.java
+++ src/main/java/app/DAOs/IDAO.java
@@ -3,5 +3,9 @@ package app.DAOs;
 public interface IDAO<Entity, DTO, ID> {
 
     Entity create(Entity entity);
-    
+
+    void delete(ID id);
+
+    Entity update(Entity entity);
+
 }
